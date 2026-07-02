2b60f3c0107b0671c121d6bd674cb533c56e24a8
Eduard Gode <eduard.gode@wdss.de>
2026-07-02 12:27:19 +0200

    stty handling by udev and usbipd under win11

--- test1.sh
+++ test1.sh
@@ -1,8 +1,23 @@
 port=/dev/ttyACM0
 
-echo -e "to show output start: \nwhile true; do cat - < $port; sleep 1; done"
+# echo -e "to show output start: \nstty -F $port raw -echo; while true; do cat - < $port; sleep 1; done"
 
-stty -F $port raw -echo
+# stty -F $port raw -echo
+
+# the stty command can be executed by systemd-udev if configured.
+#   udev config file: /etc/udev/rules.d/99-esp32-serial.rules
+#   rule in file:
+#     ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="303a", ATTRS{idProduct}=="1001", RUN+="/bin/stty -F /dev/%k raw -echo 115200" 
+#
+# reload and activate after configuration modifications:
+#   sudo udevadm control --reload-rules
+#   sudo udevadm trigger
+#
+# in windows wsl you can install usbipd and start accessing by:
+# usbipd list
+# usbipd bind --busid 2-1                       # once to bind
+# usbipd attach --wsl --busid 2-1               # for connect after each plug in
+# usbipd attach --wsl --busid 2-1 --auto-attach # for automatic reconnect
 
 while true; do
        echo -ne "\xA0\x01\x01\xA2" > $port
